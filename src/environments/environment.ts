eb0e72c0b5ebbbd0a4a7b99c97163105cb2c05c1
DayseEllen <esyadnelle@gmail.com>
2019-04-29 19:34:05 -0300

    novoCommit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCXuQlY6tKJTWTForp1p7oSy5L1QwUH4T8",
+    authDomain: "ionic4firebase-17a47.firebaseapp.com",
+    databaseURL: "https://ionic4firebase-17a47.firebaseio.com",
+    projectId: "ionic4firebase-17a47",
+    storageBucket: "ionic4firebase-17a47.appspot.com",
+    messagingSenderId: "213900127867"
+  }
 };
 
 /*
